2043f5b0079f76093bf4ba9fd6f3be60803646ed
xxblackacidxx <mattnappo@gmail.com>
2016-09-24 16:49:03 -0400

    Add multiple data types to a linked list.

--- store.py
+++ store.py
@@ -1,11 +1,24 @@
 from linkedlist import LinkedList
 pwlist = LinkedList()
 while True:
-    x = input("[V]iew, [A]ppend, [I]nsert, or [R]emove? ")
+    x = input("[V]iew, [S]tring append, [A]ppend, [I]nsert, or [R]emove? ")
     if x == "V" or x == "v":
         pwlist.printer()
-    elif x == "A" or x == "a":
+    elif x == "S" or x == "s":
         pwlist.append(input("Enter data: "))
+    elif x == "A" or x == "a":
+        dataType = input("Append [B]oolean, [S]tring, [I]nteger, or [L]ist? ")
+        if dataType == "B" or dataType == "b":
+            pwlist.append(bool(input("Enter 'True' or enter 'False': ")))
+        elif dataType == "S" or dataType == "s":
+            pwlist.append(input("Enter a string: "))
+        elif dataType == "I" or dataType == "i":
+            pwlist.append(eval(input("Enter an integer: ")))
+        elif dataType == "L" or dataType == "l":
+            print("Format: 'item1,item2,item3,item4'")
+            preList = input("Enter list: ")
+            newList = preList.split(",")
+            pwlist.append(newList)
     elif x == "I" or x == "i":
         pwlist.insert(eval(input("Enter place number: ")), input("Enter data: "))
     elif x == "R" or x == "r":
