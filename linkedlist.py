7ebb971149c16200ee7f5114c62d3732659c90f8
CoditumLabs <CoditumLabs@10.210.0.117>
2016-10-01 12:20:23 -0400

    Removed append function, self.end, fixed insert, fixed printer, and added peek function.

--- linkedlist.py
+++ linkedlist.py
@@ -5,51 +5,40 @@ class Node:
 class LinkedList:
     def __init__(self):
         self.head = None
-        self.end = None
         self.counter = 0
-    def append(self, data):
-        node = Node(data)
-        if self.counter == 0:
-            self.head = node
-            self.end = node
-            self.counter+=1
-        self.end.link = node #actually add it
-        self.end = node
-        self.counter+=1
     def insert(self, place, data):
         if self.counter == 0:
-            self.append(data)
-        newNode = Node(data)
-        node = self.head
-        for x in range(place-1):
-            node = node.link
-        newNode.link = node.link
-        node.link = newNode
-        self.counter+=1
+            newNode = Node(data)
+            self.head = newNode
+            self.counter+=1
+        elif place == 0:
+            newNode = Node(data)
+            newNode.link = self.head
+            self.head = newNode
+            self.counter+=1
+        else:
+            newNode = Node(data)
+            node = self.head
+            for x in range(place-1):
+                node = node.link
+            newNode.link = node.link
+            node.link = newNode
+            self.counter+=1
     def printer(self):
         node = self.head
-        for x in range(self.counter):
-            if node == self.end:
-                print("["+str(x)+"]"+self.end.data)
-                break
-            else:
-                print("["+str(x)+"]"+node.data)
-                #ATTEMPTING TO SHOW DATA TYPE WHEN PRINTING
-                node = node.link
+        for x in range(self.counter-1):
+            print("["+str(x)+"] "+node.data)
+            node = node.link
+        print("["+str(self.counter)+"] "+node.data)
     def remove(self, place):
         if place == 0:
             self.head = self.head.link
             self.counter-=1
-        elif place == self.counter-1:
-            node = self.head
-            for x in range(place):
-                node = node.link
-            self.head = node
         else:
             node = self.head
-            for x in range(place-1):
+            for x in range(place-2):
                 node = node.link
-            for x in range(place):
-                rmv = node.link
-            node.link = rmv.link
+            node.link = node.link.link
             self.counter-=1
+    def peek(self):
+        return self.head.data
--- store.py
+++ store.py
@@ -1,26 +1,32 @@
 from linkedlist import LinkedList
 pwlist = LinkedList()
+pwlist.insert(pwlist.counter, "one")
+pwlist.insert(pwlist.counter, "two")
+pwlist.insert(pwlist.counter, "three")
+pwlist.insert(pwlist.counter, "four")
 while True:
-    x = input("[V]iew, [S]tring append, [A]ppend, [I]nsert, or [R]emove? ")
+    x = input("[V]iew, [S]tring append, [A]ppend, [I]nsert, [R]emove, [P]eek? ")
     if x == "V" or x == "v":
         pwlist.printer()
     elif x == "S" or x == "s":
-        pwlist.append(input("Enter data: "))
+        pwlist.insert(pwlist.counter, input("Enter data: "))
     elif x == "A" or x == "a":
         dataType = input("Append [B]oolean, [S]tring, [I]nteger, or [L]ist? ")
         if dataType == "B" or dataType == "b":
-            pwlist.append(bool(input("Enter 'True' or enter 'False': ")))
+            pwlist.insert(pwlist.counter+1, bool(input("Enter 'True' or enter 'False': ")))
         elif dataType == "S" or dataType == "s":
-            pwlist.append(input("Enter a string: "))
+            pwlist.insert(pwlist.counter+1, input("Enter a string: "))
         elif dataType == "I" or dataType == "i":
-            pwlist.append(eval(input("Enter an integer: ")))
+            pwlist.insert(pwlist.counter+1, eval(input("Enter an integer: ")))
         elif dataType == "L" or dataType == "l":
             print("Format: 'item1,item2,item3,item4'")
             preList = input("Enter list: ")
             newList = preList.split(",")
-            pwlist.append(newList)
+            print(newList)
+            pwlist.insert(pwlist.self.counter+1, newList)
     elif x == "I" or x == "i":
         pwlist.insert(eval(input("Enter place number: ")), input("Enter data: "))
     elif x == "R" or x == "r":
         pwlist.remove(eval(input("Enter place number: ")))
-#UNABLE TO INSERT HEAD OR END, ONLY IN MIDDLE NODES
+    elif x == "P" or x == "p":
+        print(pwlist.peek())
